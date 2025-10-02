920c3046c300d60185855ccddd74f0fe7ad4ebd8
Akash Sagar <akashsagar@acstechconsulting.com>
2025-10-02 16:46:33 +0530

    url

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ dotenv.config();
 
 
   app.use(cors({
-    origin: 'http://localhost:5173 ',
+    origin: 'https://todoprojectsagar.netlify.app',
   methods: ['GET','POST','PUT','DELETE','OPTIONS'],
   credentials: true,
  }));
